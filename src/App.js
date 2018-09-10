01fd2d97369a61772c51b83d99c74fd3cbe95855
Paul Kögel <i.paul.koegel@gmail.com>
2018-09-10 23:23:26 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Juicer from './Juicer';
+
+class App extends Component {
+  render() {
+    return (
+      <Juicer feedId='peter-riviera-36da2f7b-c91c-4705-9f62-2485111f4c1f' />
+    );
+  }
+}
+
+export default App;
