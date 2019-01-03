f5eb17205a3dfa2845816874abf935100aab2d2f
Paul Kögel <i.paul.koegel@gmail.com>
2019-01-03 16:01:42 +0100

    App.js: add HTML around JuicerIo component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-import Juicer from './Juicer';
+import JuicerIo from './JuicerIo';
 
 class App extends Component {
   render() {
     return (
-      <Juicer feedId='peter-riviera-36da2f7b-c91c-4705-9f62-2485111f4c1f' />
+      <div>
+        <div style={{ padding: 10, borderBottom: '1px solid black' }}>
+          <h1>Juicer React Demo</h1>
+          <p>Demo app for the <a href="https://www.juicer.io" target="_blank" rel="noopener noreferrer">Juicer.io</a> React component.</p>
+        </div>
+        <JuicerIo feedId='juicer' />
+        <JuicerIo feedId='peter-riviera-36da2f7b-c91c-4705-9f62-2485111f4c1f' />
+        <footer>
+          <h3>Footer</h3>
+          <p>This should appear below the Juicer.io grid.</p>
+        </footer>
+      </div>
     );
   }
 }
