a5417b78923e3228d532342ae2a56402dfa0c111
Paul Kögel <i.paul.koegel@gmail.com>
2019-01-05 00:15:49 +0100

    App: use JuicerFeed component from npm package.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import JuicerIo from './JuicerIo';
+import JuicerFeed from 'react-juicer-feed';
 
 class App extends Component {
   render() {
@@ -9,8 +9,8 @@ class App extends Component {
           <h1>Juicer React Demo</h1>
           <p>Demo app for the <a href="https://www.juicer.io" target="_blank" rel="noopener noreferrer">Juicer.io</a> React component.</p>
         </div>
-        <JuicerIo feedId='juicer' />
-        <JuicerIo feedId='peter-riviera-36da2f7b-c91c-4705-9f62-2485111f4c1f' />
+        <JuicerFeed feedId='juicer' />
+        <JuicerFeed feedId='peter-riviera-36da2f7b-c91c-4705-9f62-2485111f4c1f' />
         <footer>
           <h3>Footer</h3>
           <p>This should appear below the Juicer.io grid.</p>
