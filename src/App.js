cc5cc9295a5aa6a755729cee32a4f2a423c2025b
Paul Kögel <i.paul.koegel@gmail.com>
2020-02-28 13:45:33 +0100

    Add link.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
       <div>
         <div style={{ padding: 10, borderBottom: '1px solid black' }}>
           <h1>Juicer React Demo</h1>
-          <p>Demo app for the <a href="https://www.juicer.io" target="_blank" rel="noopener noreferrer">Juicer.io</a> React component.</p>
+          <p>Demo app for the <a href="https://www.juicer.io" target="_blank" rel="noopener noreferrer">Juicer.io</a> <a href='https://github.com/juicer-io/react-juicer-feed/' target="_blank" rel="noopener noreferrer">React component</a>.</p>
         </div>
+
         <JuicerFeed feedId='juicer' />
+
         <footer>
           <h3>Footer</h3>
           <p>This should appear below the Juicer.io grid.</p>
