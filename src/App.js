dd3d87a0bcfb64495c5a3aec5a942f62cc27b9b6
Paul Kögel <i.paul.koegel@gmail.com>
2020-02-28 14:47:40 +0100

    add variable to make clear what 'juicer' is here.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import JuicerFeed from 'react-juicer-feed';
 
+// NOTE: Juicer demo feed ID (also see: https://www.juicer.io/juicer)
+//   Replace with your own Feed ID once you've set up a free feed on juicer.io
+const demoFeedId = 'juicer';
+
 class App extends Component {
   render() {
     return (
@@ -10,7 +14,7 @@ class App extends Component {
           <p>Demo app for the <a href="https://www.juicer.io" target="_blank" rel="noopener noreferrer">Juicer.io</a> <a href='https://github.com/juicer-io/react-juicer-feed/' target="_blank" rel="noopener noreferrer">React component</a>.</p>
         </div>
 
-        <JuicerFeed feedId='juicer' />
+        <JuicerFeed feedId={demoFeedId} />
 
         <footer>
           <h3>Footer</h3>
